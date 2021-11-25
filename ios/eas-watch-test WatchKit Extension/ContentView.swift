77b9bd62156936de9a5f6b3ef07ae473ff2b55a9
Jonas Sourlier <jonas.sourlier@yolp.ch>
2021-11-25 17:55:47 +0100

    added Watch target

---
+++ ios/eas-watch-test WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eas-watch-test WatchKit Extension
+//
+//  Created by Jonas Sourlier on 25.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/eas-watch-test WatchKit Extension/easwatchtestApp.swift
@@ -0,0 +1,19 @@
+//
+//  easwatchtestApp.swift
+//  eas-watch-test WatchKit Extension
+//
+//  Created by Jonas Sourlier on 25.11.21.
+//
+
+import SwiftUI
+
+@main
+struct easwatchtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
